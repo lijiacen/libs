8b67085e1ab5e16e3215c483bb82cbbd383809bc
lijiacen <50915968+lijiacen@users.noreply.github.com>
2021-01-07 22:51:54 +0800

    rollup打包vue

--- roolup.config.prod.js
+++ roolup.config.prod.js
@@ -4,6 +4,9 @@ const commonjs = require("rollup-plugin-commonjs");//commonjs插件,打包common
 const babel = require("rollup-plugin-babel");//babel插件，把代码转为es5，适配更多浏览器
 const json = require("@rollup/plugin-json");//打包json文件
 const { terser } = require("rollup-plugin-terser");//压缩文件
+const vue = require("rollup-plugin-vue"); //打包vue文件
+const postcss = require("rollup-plugin-postcss");//打包样式文件
+
 
 const inputPath = path.resolve(__dirname, './src/index.js');
 const outputPath = path.resolve(__dirname, './dist/index.min.js');
@@ -13,10 +16,15 @@ module.exports = {
   output: {
     file: outputPath,
     format: 'es',
-    name: "data_v"
+    name: "data_v",
+    globals: {
+      vue: 'vue'
+    }
   },
   plugins: [resolve(), commonjs(), babel({
     exclude: "node_modules/**"
-  }), json(), terser()],
-  external: ['lodash']//external中的变量，都当作外部变量引用，防止冗余
+  }), json(), terser(), vue(), postcss({
+    plugins: []
+  })],
+  external: ['lodash', 'vue']//external中的变量，都当作外部变量引用，防止冗余
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import { a, b, c } from './plugin';
-import { common_a, common_b } from './cjs';
-import _ from 'lodash';
-import pkg from '../package.json';
+// import { a, b, c } from './plugin';
+// import { common_a, common_b } from './cjs';
+// import _ from 'lodash';
+// import pkg from '../package.json';
+import Test from './Test.vue';
 
-console.log(pkg);
-c();
-console.log(common_a);
+// console.log(pkg);
+// c();
+// console.log(common_a);
+export default function (Vue) {
+  Vue.component(Test.name, Test);
+}
