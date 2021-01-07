bbd542e127f39e21871c86c0febfc5bcffa3a29f
lijiacen <50915968+lijiacen@users.noreply.github.com>
2021-01-07 21:40:49 +0800

    babel,json,terser插件

--- roolup.config.dev.js
+++ roolup.config.dev.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const resolve = require("rollup-plugin-node-resolve");//帮助Rollup查找外部模块,然后导入
 const commonjs = require("rollup-plugin-commonjs");//commonjs插件,打包commonjs代码
+const babel = require("rollup-plugin-babel");//babel插件，把代码转为es5，适配更多浏览器
+const json = require("@rollup/plugin-json");//打包json文件
 
 const inputPath = path.resolve(__dirname, './src/index.js');
 const outputPath = path.resolve(__dirname, './dist/index.js');
@@ -12,6 +14,8 @@ module.exports = {
     format: 'es',
     name: "data_v"
   },
-  plugins: [resolve(), commonjs()],
+  plugins: [resolve(), commonjs(), babel({
+    exclude: "node_modules/**"
+  }), json()],
   external: ['lodash']//external中的变量，都当作外部变量引用，防止冗余
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { a, b, c } from './plugin';
 import { common_a, common_b } from './cjs';
 import _ from 'lodash';
+import pkg from '../package.json';
 
+console.log(pkg);
 c();
 console.log(common_a);
